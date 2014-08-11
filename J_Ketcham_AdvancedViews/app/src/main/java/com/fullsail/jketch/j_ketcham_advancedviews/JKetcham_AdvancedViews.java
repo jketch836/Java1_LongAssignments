168e1ef2f8dacdccfe862d7c3b5a9f816558b662
jketch836 <jketch@JoshKs-MacBook-Pro.local>
2014-08-11 09:23:44 -0400

    AdvancedView Project Orientation
    Coded the AdvancedViews Project Orientation (it is detecting the
    orientation of the device

---
+++ J_Ketcham_AdvancedViews/app/src/main/java/com/fullsail/jketch/j_ketcham_advancedviews/JKetcham_AdvancedViews.java
@@ -0,0 +1,43 @@
+package com.fullsail.jketch.j_ketcham_advancedviews;
+
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.os.Bundle;
+
+
+public class JKetcham_AdvancedViews extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jketcham__advanced_views);
+
+        //Getting the current screen orientation and then setting the orientation of app to that.
+        int deviceOrientation = getResources().getConfiguration().orientation;
+        switch(deviceOrientation) {
+            case Configuration.ORIENTATION_PORTRAIT:
+                setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+                break;
+            case Configuration.ORIENTATION_LANDSCAPE:
+                setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+                break;
+        }
+
+    }
+
+    // Check screen orientation, then go from here
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        // Checks the orientation of the screen
+        if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
+
+
+        } else if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+
+        }
+    }
+
+}
